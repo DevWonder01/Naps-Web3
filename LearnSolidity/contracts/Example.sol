5c59082027255695d699b0b7b891cc85ec0b090d
DevWonder01 <55108486+DevWonder01@users.noreply.github.com>
2025-03-07 18:16:14 +0100

    Init

---
+++ LearnSolidity/contracts/Example.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Example {
+    int public age = 50;
+
+    string public name = "Taiwo";
+
+    string public career = "Software Engineer";
+
+    
+
+
+}
