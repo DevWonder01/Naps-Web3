9a9c66bf9421d5b5aa481947d200416d31e7fb64
ArcBase <taiwoadebayo4040@gmail.com>
2025-07-08 18:49:53 +0000

    add - app.sol from tutorial's point

--- LearnSolidity/contracts/app.sol
+++ LearnSolidity/contracts/app.sol
@@ -85,7 +85,7 @@ contract CustomArray {
     }
 
     function update_array() public {
-        array[1] = 1;
+        array[0] = 1;
         array[1] = 2;
         array[2] = 3;
         array[3] = 4;
