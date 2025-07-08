51f6e8c05e600835f20a7e9384479102dbb69603
ArcBase <taiwoadebayo4040@gmail.com>
2025-07-08 18:30:11 +0000

    add - app.sol from tutorial's point

--- LearnSolidity/contracts/app.sol
+++ LearnSolidity/contracts/app.sol
@@ -41,4 +41,69 @@ contract ContractTwo {
             return false;
         }
     }
+    uint public fixedBalanceExplicit[10] = [1, 2, 3, 0, 0, 0, 0, 0, 0, 0];
+
+
+    
+    function testArray() public pure {
+        uint256 len = 7;
+
+        //dynamic array
+        uint256[] memory a = new uint256[](7);
+
+        //bytes is same as byte[]
+        bytes memory b = new bytes(len);
+
+        assert(a.length == 7);
+        assert(b.length == len);
+
+        //access array variable
+        a[6] = 8;
+
+        //test array variable
+        assert(a[6] == 8);
+
+        //static array
+        uint256[3] memory c = [uint256(1), 2, 3];
+        assert(c.length == 3);
+    }
+
+}
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CustomArray {
+    uint256[] public lucky_array = new uint256[](7);
+
+
+    constructor() {
+
+    }
+
+    function update_array() public {
+        lucky_array[0] = 1;
+        lucky_array[1] = 2;
+        lucky_array[2] = 3;
+        lucky_array[3] = 3;
+        lucky_array[4] = 3;
+        lucky_array[5] = 3;
+        lucky_array[6] = 3;
+    }
+
+    function get_element(uint val) public view returns (uint256){
+        require(val < lucky_array.length, "Index out of bounds");
+        return lucky_array[val];
+    }
+
+    function set_element(uint index, uint val) public returns (uint256){
+        require(index < lucky_array.length, "Index out of bounds");
+        lucky_array[index] = val;
+        return lucky_array[index];
+    }
+
+    function get_array_length() public view returns (uint256) {
+        return lucky_array.length;
+    }
 }
